dd158805e9f1bb014c4219b4fcd7c907d09d968a
Lukas Reindl <lukas.reindl@innoactive.de>
2017-05-03 13:58:58 +0200

    fire loop and properties

---
+++ Assets/Scripts/Weapons/NewSystem/Properties.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Weapons {
+
+    [Serializable]
+    public class Properties {
+
+        /// <summary>
+        /// Time need pressed before shot
+        /// </summary>
+        public float loadingTime;
+
+        /// <summary>
+        /// Time beween shots
+        /// </summary>
+        public float cooldownTime;
+
+        /// <summary>
+        /// Time to fill up clip
+        /// </summary>
+        public float reloadTime;
+
+        /// <summary>
+        /// Can hold the button down
+        /// </summary>
+        public bool isContinuousFire = false;
+
+        /// <summary>
+        /// Amount of shots per button press
+        /// </summary>
+        public int salveCount = 1;
+
+        /// <summary>
+        /// How many times can we shoot before we have to reload
+        /// </summary>
+        public int clipSize = 6;
+    }
+}
