caa113e27db275a26d0266658417b0babb5d7edf
Lukas Reindl <lukas.reindl@innoactive.de>
2017-05-09 17:41:01 +0200

    added postprocessing effects, dummy player movement

---
+++ Assets/Scripts/PlayerMovement/AgentFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentFollow : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    float minDist = 1;
+
+    NavMeshAgent agent;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Follow();
+    }
+
+    private void Follow()
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) < minDist)
+        {
+            return;
+        }
+
+        agent.SetDestination(target.transform.position);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement/PlayerMovementNavMesh.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovementNavMesh : MonoBehaviour {
+
+    public float speed = 80;
+    NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        //pawn.transform.Translate(new Vector3(x, 0, y) * Time.deltaTime * pawnSpeed, Space.World);
+        agent.SetDestination(transform.position + new Vector3(x, 0, y) * Time.deltaTime * speed);
+    }
+}
