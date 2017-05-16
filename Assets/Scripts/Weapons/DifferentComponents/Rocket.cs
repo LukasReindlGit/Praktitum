14df0ad5863f0871f60f478a360076ad0d66f5fa
Lukas Reindl <lukas.reindl@innoactive.de>
2017-05-16 17:37:56 +0200

    rockets started, parameter

---
+++ Assets/Scripts/Weapons/DifferentComponents/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Rocket : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 10;
+
+    Rigidbody rigid;
+
+    [SerializeField]
+    Transform target;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if(target)
+        {
+            transform.LookAt(target.transform.position);
+        }
+
+        rigid.velocity = (transform.forward * speed);
+        
+    }
+}
