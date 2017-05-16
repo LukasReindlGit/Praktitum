3d763419e38bdc37324ccaddf7fcd2faf0532e28
Lukas Reindl <lukas.reindl@innoactive.de>
2017-05-16 18:27:27 +0200

    rockets explode

---
+++ Assets/Scripts/Weapons/DifferentComponents/ExplodeOnCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnCollision : MonoBehaviour {
+
+    [SerializeField]
+    GameObject explosion;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
