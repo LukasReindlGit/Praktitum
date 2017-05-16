d629ee3bf80b9a794ad9f35d4f38b94bb69d7518
Maximilian Hotter <hotter.maximilian@web.de>
2017-05-16 17:44:43 +0200

    Tuned System

--- Assets/Scripts/Targeting/TargetingImplementations/NearestEnemySpherical.cs
+++ Assets/Scripts/Targeting/TargetingImplementations/NearestEnemySpherical.cs
@@ -12,10 +12,11 @@ public class NearestEnemySpherical : MonoBehaviour
     public float playersNearViewAngle = 15f;
     public float immediateProximity = 15f;
     
+    private GameObject[] sortedEnemies;
 
     private List<GameObject> result;
     private Collider[] enemies;
-    private float range = 50f;
+    private float range;
     private bool nearEnemyInView = false;
     private bool distantEnemyInView = false;
     private float distanceToEnemy;
@@ -26,42 +27,26 @@ public class NearestEnemySpherical : MonoBehaviour
     {
         enemyMask = LayerMask.GetMask("Shootable");
         rayMask = LayerMask.GetMask("Shootable", "Obstacle");
+        result = new List<GameObject>();
+        GameHandler.OnGizmoDrawEvent += DrawGizmos;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        GameObject[] obj = FindObjectsOfType<GameObject>();
 
-        for (int i = 0; i < obj.Length; i++)
-        {
-            if (obj[i].layer == LayerMask.NameToLayer("Shootable"))
-            {
-                obj[i].GetComponent<MeshRenderer>().material.color = Color.gray;
-            }
-        }
 
         GameObject[] list = getNearestEnemies(transform.position, transform.forward, 20);
-        for (int i = 0; i < list.Length; i++)
-        {
-            list[i].GetComponent<MeshRenderer>().material.color = Color.red;
-        }
-
-        GameObject n = getTargetEnemy(list);
-        if (n != null) {
-            n.GetComponent<MeshRenderer>().material.color = Color.green;
-        }
         
-
     }
 
-    public GameObject[] getNearestEnemies(Vector3 position, Vector3 direction, float maxDistance)
+    public void updateNearestEnemies(Vector3 position, Vector3 direction, float maxDistance)
     {
         // Search for all enemies in a sphere around the weapon
         range = maxDistance / 2.0f;
         enemies = Physics.OverlapSphere(position + new Vector3(direction.x, 0, direction.z).normalized * range, range, enemyMask);
 
-        List<GameObject> result = new List<GameObject>();
+        result.Clear();
         for (int i = 0; i < enemies.Length; i++)
         {
             // Get all enemies in Sight
@@ -71,7 +56,7 @@ public class NearestEnemySpherical : MonoBehaviour
             }
         }
 
-        result.Sort( (a, b) => {
+        result.Sort((a, b) => {
             if ((a.transform.position - position).sqrMagnitude > (b.transform.position - position).sqrMagnitude)
             {
                 return 1;
@@ -86,7 +71,17 @@ public class NearestEnemySpherical : MonoBehaviour
             }
         });
 
-        return result.ToArray();
+        sortedEnemies = result.ToArray();
+    }
+
+    public GameObject[] getNearestEnemies(Vector3 position, Vector3 direction, float maxDistance)
+    {
+        updateNearestEnemies(position, direction, maxDistance);
+        return getNearestEnemies();
+    }
+    public GameObject[] getNearestEnemies()
+    {
+        return sortedEnemies;
     }
 
     public GameObject getTargetEnemy(Vector3 position, Vector3 direction, float maxDistance)
@@ -139,13 +134,16 @@ public class NearestEnemySpherical : MonoBehaviour
 
     private void DrawGizmos()
     {
+        // Sphere
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position + transform.forward.normalized * range, range);
 
+        // Local Z-Axis (View-Axis)
         Gizmos.color = Color.blue;
         Vector3 distPos = transform.position + transform.forward.normalized * range * 2;
         Gizmos.DrawLine(transform.position, distPos);
 
+        // Full View Angle
         Vector3 distPosRight = Quaternion.Euler(0, playersViewAngle, 0) * (distPos - transform.position) + transform.position;
         Vector3 distPosLeft = Quaternion.Euler(0, -playersViewAngle, 0) * (distPos - transform.position) + transform.position;
         Gizmos.color = Color.green;
@@ -153,6 +151,7 @@ public class NearestEnemySpherical : MonoBehaviour
         Gizmos.DrawLine(transform.position, distPosLeft);
         Gizmos.DrawLine(distPosLeft, distPosRight);
 
+        // Proximity View Angle
         Vector3 distPosNear = transform.position + transform.forward.normalized * immediateProximity;
         Vector3 distPosNearRight = Quaternion.Euler(0, playersNearViewAngle, 0) * (distPosNear - transform.position) + transform.position;
         Vector3 distPosNearLeft = Quaternion.Euler(0, -playersNearViewAngle, 0) * (distPosNear - transform.position) + transform.position;
@@ -160,5 +159,20 @@ public class NearestEnemySpherical : MonoBehaviour
         Gizmos.DrawLine(transform.position, distPosNearRight);
         Gizmos.DrawLine(transform.position, distPosNearLeft);
         Gizmos.DrawLine(distPosNearLeft, distPosNearRight);
+
+        // Mark enemies (green = target, red = other seen enemies)
+        int length = sortedEnemies.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if (i != 0)
+            {
+                Gizmos.color = Color.red;
+            } else
+            {
+                Gizmos.color = Color.green;
+            }
+            
+            Gizmos.DrawWireCube(sortedEnemies[i].transform.position, sortedEnemies[i].GetComponent<Renderer>().bounds.size);
+        }
     }
 }
