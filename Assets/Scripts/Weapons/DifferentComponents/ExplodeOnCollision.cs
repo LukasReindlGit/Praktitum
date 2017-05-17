632aadad3a4ead19e6344d7e4f6d118be1c2739d
Lukas Reindl <lukas.reindl@innoactive.de>
2017-05-17 17:18:33 +0200

    nice trails and voxel explosions

--- Assets/Scripts/Weapons/DifferentComponents/ExplodeOnCollision.cs
+++ Assets/Scripts/Weapons/DifferentComponents/ExplodeOnCollision.cs
@@ -6,9 +6,23 @@ public class ExplodeOnCollision : MonoBehaviour {
 
     [SerializeField]
     GameObject explosion;
-    
+
+    [SerializeField]
+    bool onTrigger = true;
     private void OnCollisionEnter(Collision collision)
     {
+        if (onTrigger)
+            return;
+
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!onTrigger)
+            return;
+
         Instantiate(explosion, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
