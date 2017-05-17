af9b6385d897887cf0076775ceceedf417111726
Lukas Reindl <lukas.reindl@innoactive.de>
2017-05-17 16:27:56 +0200

    rocket launcher

---
+++ Assets/Scripts/Weapons/NewSystem/WeaponAcceleratedProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Weapons
+{
+    public class WeaponAcceleratedProjectile : WeaponProjectileBehaviour
+    {
+        public float projectileVelocity = 5;
+
+        public override void PerformShoot()
+        {
+            GameObject proj = SpawnProjectile();
+            AccelerateProjectile(proj);
+        }
+
+        private void AccelerateProjectile(GameObject proj)
+        {
+            Rigidbody r = proj.GetComponent<Rigidbody>();
+            if(r!=null)
+            {
+                r.velocity = Vector3.forward * projectileVelocity;
+            }
+        }
+    }
+}
