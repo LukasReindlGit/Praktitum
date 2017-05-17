a30cd7b82c5de8469e8ec44e18ed74c0dadfd721
Daniel <dyrda@in.tum.de>
2017-05-17 16:56:28 +0200

    [1] AnimationClips added [2] Basic Character Animator implemented [3] Dummy Scripts for AnimatorController & Camera Behaviour added

---
+++ Assets/Scripts/Camera/CameraBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraBehaviour : MonoBehaviour {
+
+    [Header("Basic Parameters")]
+
+    public float viewAngle;
+    public float distance;
+
+    [Header("References")]
+    public Transform characterCenter;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.rotation = Quaternion.Euler(new Vector3(90 - viewAngle, 0, 0)); 
+
+        this.transform.position = characterCenter.position - (transform.forward * distance);
+
+    }
+}
