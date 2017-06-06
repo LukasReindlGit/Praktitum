22d2f4dce6b28e8ef1e46e833dbb740370095924
Maximilian Hotter <hotter.maximilian@web.de>
2017-06-06 19:55:08 +0200

    TargetPoint basic functions (getRandomPointonSurface, IsInShootingRange)

--- Assets/Scripts/Targeting/TargetPoint.cs
+++ Assets/Scripts/Targeting/TargetPoint.cs
@@ -4,18 +4,48 @@ using UnityEngine;
 
 public class TargetPoint : MonoBehaviour {
 
+    public enum PrimitiveTypes
+    {
+        cuboid,
+        circle
+    };
+
+    public PrimitiveTypes primitiveType;
     public bool critical = false;
-    public float shootableDegree = 90;
+    public float shootableAngle = 90;
 
 	private TargetPointManager manager;
+    private Vector3 shootDirection;
+    private float angleOfShootDirection;
 
-    public void setTargetPointManager(TargetPointManager m)
+    public void setTargetPointManager(TargetPointManager manager)
     {
-        manager = m;
+        this.manager = manager;
     }
 
     public TargetPointManager getTargetPointManager()
     {
         return manager;
+    }
+
+    public Vector3 getRandomHitPointOnSurface()
+    {
+        switch (primitiveType)
+        {
+            case PrimitiveTypes.cuboid:
+                return transform.TransformPoint(Random.value - 0.5f, Random.value - 0.5f, 0);
+            case PrimitiveTypes.circle:
+                Vector2 random = Random.insideUnitCircle;
+                return transform.TransformPoint(random.x, random.y, 0);
+            default:
+                return transform.position;
+        }
+    }
+
+    public bool isInShootingAngle(Vector3 posWeapon)
+    {
+        shootDirection = transform.position - posWeapon;
+        angleOfShootDirection = Vector3.Angle(shootDirection, transform.forward);
+        return (angleOfShootDirection <= shootableAngle);
     }
 }
