2ea5dcf2a0e34acf1353e3f8f3863110b0bf74ba
Irene Herrero <herreroi@in.tum.de>
2017-06-20 16:04:13 +0200

    enemies

---
+++ Assets/Scripts/Enemy/Movementtype/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+abstract class Movement : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+    public void goTo(Transform target)
+    {
+    agent.SetDestination(agent.transform.position);
+    }
+
+    public void goToRange(Transform target, float maxDistance)
+    {
+     
+    }
+	
+}
