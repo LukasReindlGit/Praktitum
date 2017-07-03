31507921c8141d20cf263eeedc8a475714dbc0ad
Lukas Reindl <lukas.reindl@innoactive.de>
2017-07-03 11:14:44 +0200

    materials, enable disable

---
+++ Assets/Scripts/Enemy/Components/Activateables/EnableDisableComponents.cs
@@ -0,0 +1,23 @@
+﻿using AI.Component;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public abstract class EnableDisableComponents  : MonoBehaviour, IActivateable {
+
+    [SerializeField]
+    bool enable = true;
+
+    [SerializeField]
+    MonoBehaviour[] targets;
+
+    public virtual void Activate(ActivateableState state = ActivateableState.NONE)
+    {
+        foreach(var t in targets)
+        {
+            t.enabled = enable;
+        }
+    }
+    
+}
