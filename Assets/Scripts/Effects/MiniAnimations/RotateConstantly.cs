863455eb3ac0771b6d6a00736f6d3e34e86ec468
Lukas Reindl <lukas.reindl@innoactive.de>
2017-07-11 15:16:55 +0200

    added one animated projector

---
+++ Assets/Scripts/Effects/MiniAnimations/RotateConstantly.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateConstantly : MonoBehaviour {
+
+    [SerializeField]
+    Vector3 axis = Vector3.up;
+
+    [SerializeField]
+    float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(axis * speed * Time.deltaTime, Space.Self);
+	}
+}
